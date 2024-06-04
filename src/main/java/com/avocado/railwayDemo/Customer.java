037263d33e05e944fdd77a0ffa327e8abf3bb04a
Tan Thinh <thinhhtse183224@fpt.edu.vn>
2024-06-04 23:04:34 +0700

    update

---
+++ src/main/java/com/avocado/railwayDemo/Customer.java
@@ -0,0 +1,15 @@
+package com.avocado.railwayDemo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+    private String name;
+    private int age;
+}
